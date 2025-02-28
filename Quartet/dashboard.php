f2d7eef19ef03e3da4c0afaa175139128731a6af
Kyle <kbm190@icloud.com>
2025-02-28 05:57:06 -0600

    can logout to reset session, link back from dashboard to customer page

--- Quartet/dashboard.php
+++ Quartet/dashboard.php
@@ -1,4 +1,3 @@
-
 <?php
 ?>
 <!DOCTYPE html>
@@ -7,10 +6,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dashboard</title>
+    <title>Barber Dashboard</title>
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
     <p>barber dashboard</p>
+    <button onclick="location.href='index.php'">Back to Customer Site</button>
+    <form method="post" action="logout.php">
+    <button type="submit" name="logout">Logout</button>
+    </form>
   </body>
 </html>
