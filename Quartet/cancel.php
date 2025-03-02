2880ed7600cb912ce179caa776f75983f666756a
JoseLeyba <leybajoseluis@ku.edu>
2025-03-02 10:28:40 -0600

    Added User messaage when cancelling, shows user it canceled corrrectly

--- Quartet/cancel.php
+++ Quartet/cancel.php
@@ -52,6 +52,30 @@ $stmt->close();
 $deleteStmt->close();
 $mysqli->close();
 // Sends you back to home once done
-header("Location: index.php");
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            text-align: center;
+            font-family: Arial, sans-serif;
+            padding: 50px;
+        }
+
+    </style>
+    <script>
+        setTimeout(function() {
+            window.location.href = 'index.php'; 
+        }, 3000);
+    </script>
+</head>
+<body>
+    <h1>Cancellation Status</h1>
+    <p>Canceled Correctly! Redirecting to Home Page...</p>
+
+</body>
+</html>
