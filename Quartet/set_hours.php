26912e548d78e9a6300f1224b6ecc35b907c59fd
brinley53 <brinleyhull53@ku.edu>
2025-04-15 18:24:29 -0500

    delete unnecessary

--- Quartet/set_hours.php
+++ Quartet/set_hours.php
@@ -41,7 +41,7 @@ include("barber_header.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/barber.css">
+    <link rel="stylesheet" href="style/barber_style.css">
     <title>Barber Calendar</title>
     <style>
         /* Times grid styling */
--- Quartet/set_hours_manager.php
+++ Quartet/set_hours_manager.php
@@ -62,7 +62,7 @@ else {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/barber.css">
+    <link rel="stylesheet" href="style/barber_style.css">
     <title>Barber Calendar</title>
     <style>
         /* Times grid styling */
